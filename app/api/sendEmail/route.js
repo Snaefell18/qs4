734219ac7f3fb35aec3094be897eff99e7add17c
Snaefell18 <jan.rentzsch@googlemail.com>
2025-10-27 15:52:12 +0100

    Update route.js

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -35,7 +35,7 @@ export async function POST(req) {
     // E-Mail senden
     const data = await resend.emails.send({
       from: "Exchange Rates-App <mail@qs4-org.org>", // dein neuer Domain-Absender
-      to: "j.rentzsch@euroimmun.de", // Standardempfänger
+      to: "a.minardi@euroimmun.de", // Standardempfänger
       subject: `WSJ-Wechselkurse vom ${datum}`,
       html,
     });
