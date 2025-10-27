84955328f0a17c40d6f15ebe266fe58cb9ce53c7
Snaefell18 <jan.rentzsch@googlemail.com>
2025-10-27 12:33:38 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,12 @@
 export const metadata = {
-  title: "Microlink Screenshot App",
-  description: "Eine kleine Demo-App mit Next.js + Vercel",
+  title: "Wechselkurse-Screenshot-App",
+  description: "Microlink + Vercel Blob Demo",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
-      <body
-        style={{
-          fontFamily: "sans-serif",
-          backgroundColor: "#f9fafb",
-          color: "#111",
-          margin: 0,
-          padding: 0,
-        }}
-      >
+      <body>
         {children}
       </body>
     </html>
