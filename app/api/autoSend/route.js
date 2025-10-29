66efcb678482c2557e931fb17a6297192f24c5ce
Snaefell18 <jan.rentzsch@googlemail.com>
2025-10-29 08:57:28 +0100

    Update route.js

--- app/api/autoSend/route.js
+++ app/api/autoSend/route.js
@@ -92,7 +92,7 @@ export async function GET() {
     const resend = new Resend(process.env.RESEND_API_KEY);
     await resend.emails.send({
       from: "Exchange Rates-App <mail@qs4-org.org>",
-      to: "jn.rentzsch@euroimmun.de",
+      to: "a.minardi@euroimmun.de",
       subject: `WSJ-Wechselkurse vom ${datum}`,
       html,
     });
